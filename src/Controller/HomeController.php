8f4ca68da9e10508fba77fd9221c308867cb0d42
Sh-Daria <shmygareva_daria@mail.ru>
2024-01-05 01:25:25 +0700

    ArticleServiceInterface - solid архитектура

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\ArticleRepository;
-use App\Service\ArticleService;
+USE App\Service\ArticleServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +12,7 @@ class HomeController extends AbstractController
     private const RECENT_ARTICLE_COUNT_ON_HOME = 5;
 
     #[Route('/', name: 'homepage')]
-    public function index(ArticleService $articleService): Response
+    public function index(ArticleServiceInterface $articleService): Response
     {
         // Controller поднимает бизнес-сервис по приходу (получению) запроса, который в свою очередь выполняет логику и возвращает ответ
         return $this->render('home/index.html.twig', [
--- src/Service/ArticleService.php
+++ src/Service/ArticleService.php
@@ -3,17 +3,21 @@
 namespace App\Service;
 
 use App\Repository\ArticleRepository;
+use Psr\Log\LoggerInterface;
 
-class ArticleService
+class ArticleService implements ArticleServiceInterface
 {
-    public function __construct(private readonly ArticleRepository $articleRepository)
+    public function __construct(
+        private readonly ArticleRepository $articleRepository,
+        private readonly LoggerInterface $logger,
+        )
     {
 
     }
 
-
-    public function getRecentArticles(int $count)
+    public function getRecentArticles(int $count) : array
     {
+        $this->logger->info(sprintf('getting %d recent articles', $count));
         // место для дополнительного функционала перед отправкой (матем. модель, кеш...)
         return $this->articleRepository->getRecentArticles($count);
     }
