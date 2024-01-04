5caed0882b3d0a43c38bd575147a4250c057fdd6
Sh-Daria <shmygareva_daria@mail.ru>
2024-01-05 00:06:14 +0700

    Бизнес логика (добавление промежуточного слоя ArticleService)

---
+++ src/Service/ArticleService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\ArticleRepository;
+
+class ArticleService
+{
+    public function __construct(private readonly ArticleRepository $articleRepository)
+    {
+
+    }
+
+
+    public function getRecentArticles(int $count)
+    {
+        return $this->articleRepository->getRecentArticles($count);
+    }
+}
